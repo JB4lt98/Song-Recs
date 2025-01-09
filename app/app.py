567d4bf2be3bdba616243423447666c50cb77a43
JB4lt98 <jbalt4zar@gmail.com>
2025-01-08 21:49:03 -0500

    File upload
    Dont worry about this, just putting the curtains up :)

--- app/app.py
+++ app/app.py
@@ -2,6 +2,13 @@ from flask import Flask, render_template
 from flask import Flask, request
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 
 app = Flask(__name__)
 
